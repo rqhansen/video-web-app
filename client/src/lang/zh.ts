c5f14f1e741f69632cee1fdc995a8d08763cadef
rqhansen <1508477509@qq.com>
2019-10-01 22:43:42 +0800

    '添加中文文字'

--- client/src/lang/zh.ts
+++ client/src/lang/zh.ts
@@ -6,6 +6,101 @@ export default  {
         weekMovie: '近一周最新电影',
         saveWebSite: '收藏本站',
         placeholder: '输入电影名称搜索',
-        downLoadUnlimitThunder: '下载无限制版迅雷'
+        downLoadUnlimitThunder: '下载无限制版迅雷',
+        search: '搜索'
+    },
+    search: {
+        searchResult: '搜索结果',
+        emptyData: '暂无资源'
+    },
+    nav: {
+        home: '首页',
+        action: '动作片',
+        comedy: '喜剧片',
+        romance: '爱情片',
+        science: '科幻片',
+        drama: '剧情片',
+        suspense: '悬疑片',
+        war: '战争片',
+        thrill: '惊悚片',
+        horror: '恐怖片',
+        disaster: '灾难片',
+        cartoon: '动画片'
+    },
+    todayMovie: {
+        todayMovieIntroduce: '今日热门电影推荐',
+        more: '更多'
+    },
+    titleBar: {
+        currLocation: '当前位置',
+        siteName: '万寻电影',
+        thunderDownLoadPage: '迅雷下载页面',
+        DownLoadPage: '下载页面',
+        shareMovie: '分享本影片给朋友',
+        copy: '点此即可复制'
+    },
+    movieList: {
+        year: '年代',
+        actor: '主演',
+        briefIntroduce: '简介',
+        updateTime: '更新时间',
+        clickToDownLoad: '点击下载',
+    },
+    movieDetail: {
+        movieName: '电影名称',
+        pubDate: '发布时间',
+        downLoadAddressAndDrama: '迅雷下载地址和剧情',
+        transName: '译名',
+        subPureName: '片名',
+        year: '年代',
+        country: '产地',
+        type: '类别',
+        language: '语言',
+        caption: '字幕',
+        onDate: '上映日期',
+        imdbScore: 'IMDb评分',
+        imdbLink: 'IMDb链接',
+        doubanScore: '豆瓣评分',
+        doubanLink: '豆瓣链接',
+        director: '导演',
+        editor: '编辑',
+        actor: '演员',
+        label: '标签',
+        briefIntro: '简介',
+        getAward: '获奖情况',
+        downLoadUrl: '下载地址',
+        downLoadTip: '温馨提示,使用迅雷您可以边下边播，如遇迅雷下载出错可换用',
+        noLimitThunder: '无限制版迅雷'
+    },
+    downLoad: {
+        thunder: '迅雷5',
+        officeNoLimit: '官方无限制版',
+        scanLesson: '查看下载教程',
+        pcDownLoad: '迅雷5PC版下载',
+        androidDownLoad: '迅雷5安卓APK下载'
+    },
+    downLoadLesson: {
+        title: '迅雷五下载教程',
+        step1: '新版迅雷中,右键点击出错的下载文件,弹出菜单中选择：种子文件另存为',
+        step2: '将种子另存为.torrent文件,点击保存，生成如下的种子文件',
+        step3: '打开下载的迅雷5.exe文件',
+        step4: '选择文件--打开BT种子文件',
+        step5: '种子标题可选输入,点击确定按钮',
+        step6: '开始下载'
+    },
+    page: {
+        total: '共',
+        goHead: '前往',
+        page: '页',
+    },
+    switchTheme: {
+        switchTheme: '更换主题',
+        cancelBtn: '取消',
+        okBtn: '确定'
+    },
+    messages: {
+        save: '请使用Ctrl+D进行添加',
+        switchThemeSuccess: '更换成功',
+        copyUrl: '网址已复制,可以分享给好友了!'
     }
 }
