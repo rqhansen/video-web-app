1c6c627b0abf00a9d2c95c734d41121b23eac20c
rqhansen <1508477509@qq.com>
2019-12-12 23:17:16 +0800

    '暂存'

--- h5/src/router.js
+++ h5/src/router.js
@@ -4,7 +4,7 @@ import Router from 'vue-router';
 const Home = () => import(/* webpackChunkName: "home" */ './views/home/Index.vue');
 const MovieDetail = () => import(/* webpackChunkName: "movieDetail" */ './views/movieDetail/Index.vue');
 const Search = () => import(/* webpackChunkName: "search" */ './views/search/Index.vue');
-const MoreMovie = () => import(/* webpackChunkName: "search" */ './views/moreMovie/Index.vue');
+const MoreMovie = () => import(/* webpackChunkName: "MoreMovie" */ './views/moreMovie/Index.vue');
 Vue.use(Router);
 
 export default new Router({
