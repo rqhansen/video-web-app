8ab17f4306106205e5ef87bb52d78576a8060c05
rqhansen <1508477509@qq.com>
2019-12-12 23:08:03 +0800

    '增加更多电影页面'

--- h5/src/router.js
+++ h5/src/router.js
@@ -4,7 +4,7 @@ import Router from 'vue-router';
 const Home = () => import(/* webpackChunkName: "home" */ './views/home/Index.vue');
 const MovieDetail = () => import(/* webpackChunkName: "movieDetail" */ './views/movieDetail/Index.vue');
 const Search = () => import(/* webpackChunkName: "search" */ './views/search/Index.vue');
-
+const MoreMovie = () => import(/* webpackChunkName: "search" */ './views/moreMovie/Index.vue');
 Vue.use(Router);
 
 export default new Router({
@@ -25,6 +25,11 @@ export default new Router({
             path: '/search',
             name: 'search',
             component: Search
+        },
+        {
+            path: '/moremovie',
+            name: 'moreMovie',
+            component: MoreMovie
         }
     ]
 })
