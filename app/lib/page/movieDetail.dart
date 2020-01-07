2062220eba240243d5e76fc6ba9bbe3a26ba0061
rq <1508477509@qq.com>
2020-01-07 18:07:25 +0800

    '暂存'

--- app/lib/page/movieDetail.dart
+++ app/lib/page/movieDetail.dart
@@ -1,6 +1,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:load/load.dart';
+import 'package:video_app/constant/netConfig.dart';
 import 'package:video_app/utils/request.dart';
 import 'package:video_app/widgets/appBar.dart';
 import 'package:video_app/utils/adapt.dart';
@@ -9,7 +10,6 @@ import 'package:video_app/redux/appState.dart';
 import 'package:video_app/widgets/dropMenu.dart';
 import 'package:video_app/widgets/footer.dart';
 import 'package:video_app/widgets/indicatorButton.dart';
-import 'package:video_app/constant/Colors.dart';
 
 class MovieDetail extends StatefulWidget {
   /// 电影的url
@@ -68,6 +68,7 @@ class _MovieDetailState extends State<MovieDetail> {
 
   @override
   Widget build(BuildContext context) {
+    if(movieInfo['detailImgSrc'] == null) return Text('');
     return Scaffold(
       appBar:CustomAppBar(
           barHeight: Adapt.px(90.0),
@@ -76,27 +77,32 @@ class _MovieDetailState extends State<MovieDetail> {
         builder: (context,showDropMenu) {
           return SingleChildScrollView(
             controller: _controller,
-            child: Stack(
-              children: <Widget>[
-                ///这个控件的作用，是将可能高度不受限制的child，调整到一个合适并且合理的尺寸
-                IntrinsicHeight(
-                  child: Container(
-                    child: Column(
-                      children: <Widget>[
-                        Container(
-                            padding: EdgeInsets.only(left: Adapt.px(8.0),top: Adapt.px(30.0),right: Adapt.px(8.0),bottom: Adapt.px(10.0),),
-                            child: DecoratedBox(
+            child: Offstage(
+              offstage: !(movieInfo['fullName'] != null),
+              child: Container(
+                child: Stack(
+                  children: <Widget>[
+                    Container(
+                      child: Column(
+                        children: <Widget>[
+                          Padding(
+                            padding: EdgeInsets.only(left: Adapt.px(16.0),top: Adapt.px(30.0),right: Adapt.px(16.0),bottom: Adapt.px(10.0),),
+                            child: Container(
                               decoration: BoxDecoration(
-                                border: Border.all(
-                                  color: Theme.of(context).primaryColor,
-                                  width: Adapt.onePx(),
-                                  style: BorderStyle.solid,
-                                ),
+                              border: Border.all(
+                                color: Theme.of(context).primaryColor,
+                                width: Adapt.onePx(),
+                                style: BorderStyle.solid,
                               ),
+                            ),
                               child: Column(
                                 children: <Widget>[
                                   Container(
+                                    height: Adapt.px(60.0),
                                     padding: EdgeInsets.symmetric(horizontal: Adapt.px(10.0)),
+                                    decoration: BoxDecoration(
+                                      color: Color.fromRGBO(247, 247, 247, 1),
+                                    ),
                                     child: Row(
                                       crossAxisAlignment: CrossAxisAlignment.center,
                                       children: <Widget>[
@@ -109,7 +115,85 @@ class _MovieDetailState extends State<MovieDetail> {
                                         GestureDetector(
                                           child: Text('${movieInfo['typeName']}',style: TextStyle(color: Theme.of(context).primaryColor),),
                                         ),
-                                        Text('>>${movieInfo['pureName']}下载页面'),
+                                        Expanded(
+                                          child: Text('>>${movieInfo['pureName']}下载页面',overflow: TextOverflow.ellipsis,),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                  Divider(
+                                    color: Color.fromRGBO(191, 228, 255, 1),
+                                    height: Adapt.onePx(),
+                                  ),
+                                  Container(
+                                    padding: EdgeInsets.only(left: Adapt.px(10.0),top: Adapt.px(15.0),right: Adapt.px(10.0),bottom: Adapt.px(30.0)),
+                                    child: Column(
+                                      children: <Widget>[
+                                        Center(
+                                          child: Text("${movieInfo['fullName']}"),
+                                        ),
+                                        Container(
+                                          margin: EdgeInsets.only(top: Adapt.px(15.0),bottom: Adapt.px(20.0),),
+                                          child: Wrap(
+                                            crossAxisAlignment: WrapCrossAlignment.center,
+                                            children: <Widget>[
+                                              Text('片名：'),
+                                              Text('${movieInfo['pureName']}',),
+                                              Text('发布时间: '),
+                                              Text('${movieInfo['pubDate']}'),
+                                            ],
+                                          ),
+                                        ),
+                                        Container(
+                                          width: Adapt.screenW(),
+                                          margin: EdgeInsets.only(bottom: Adapt.px(15.0)),
+                                          child: Wrap(
+                                            alignment: WrapAlignment.start,
+                                            children: <Widget>[
+                                              Text('${movieInfo['pureName']}迅雷下载地址和剧情: '),
+                                              GestureDetector(
+                                                child: Text('点此分享本影片',style: TextStyle(color: Theme.of(context).primaryColor),),
+                                                onTap: () {},
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                        Container(
+                                          padding: EdgeInsets.only(bottom: Adapt.px(15.0),),
+                                          child: Image.network(
+                                            '${NetConfig.baseUrl}/common/${movieInfo['detailImgSrc']}',
+                                            fit: BoxFit.cover,
+                                            width: Adapt.screenW(),
+                                            height: Adapt.px(789),
+                                          ),
+                                        ),
+                                        Container(
+                                            child: Column(
+                                              crossAxisAlignment: CrossAxisAlignment.start,
+                                              children: <Widget>[
+                                                sigleRowIntro(!(movieInfo['transName']!=null),'◎译 名',movieInfo['transName']),
+                                                sigleRowIntro(!(movieInfo['subPureName']!=null),'◎片 名',movieInfo['subPureName']),
+                                                sigleRowIntro(!(movieInfo['year']!=null),'◎年 代',movieInfo['year']),
+                                                sigleRowIntro(!(movieInfo['country']!=null),'◎产 地',movieInfo['country']),
+                                                sigleRowIntro(!(movieInfo['types']!=null),'◎类 别',movieInfo['types']),
+                                                sigleRowIntro(!(movieInfo['language']!=null),'◎语 言',movieInfo['language']),
+                                                sigleRowIntro(!(movieInfo['caption']!=null),'◎字 幕',movieInfo['caption']),
+                                                sigleRowIntro(!(movieInfo['onDate']!=null),'◎上映日期',movieInfo['onDate']),
+                                                sigleRowIntro(!(movieInfo['imdbScore']!=null),'◎IMDb评分',movieInfo['imdbScore']),
+                                                sigleRowIntro(!(movieInfo['imdbLink']!=null),'◎IMDb链接',movieInfo['imdbLink']),
+                                                sigleRowIntro(!(movieInfo['format']!=null),'◎格 式',movieInfo['format']),
+                                                sigleRowIntro(!(movieInfo['fileSize']!=null),'◎文件大小',movieInfo['fileSize']),
+                                                sigleRowIntro(!(movieInfo['videoSize']!=null),'◎视频尺寸',movieInfo['videoSize']),
+                                                sigleRowIntro(!(movieInfo['filmLength']!=null),'◎片 长',movieInfo['filmLength']),
+                                                sigleRowIntro(!(movieInfo['doubanScore']!=null),'◎豆瓣评分',movieInfo['doubanScore']),
+                                                sigleRowIntro(!(movieInfo['doubanLink']!=null),'◎豆瓣链接',movieInfo['doubanLink']),
+                                                sigleRowIntro(!(movieInfo['director']!=null),'◎导 演',movieInfo['director']),
+                                                sigleRowIntro(!(movieInfo['editor']!=null),'◎编 剧',movieInfo['editor']),
+                                                sigleRowIntro(!(movieInfo['actor']!=null),'◎主 演',movieInfo['actor']),
+                                                sigleRowIntro(!(movieInfo['label']!=null),'◎标 签',movieInfo['label']),
+                                              ],
+                                            ),
+                                        ),
                                       ],
                                     ),
                                   ),
@@ -117,33 +201,80 @@ class _MovieDetailState extends State<MovieDetail> {
                               ),
                             ),
                           ),
-                        Footer(),
-                      ],
+                          Footer(),
+                        ],
+                      ),
                     ),
-                  ),
-                ),
-
-                Positioned(
-                  top: Adapt.px(0),
-                  left: 0,
-                  width: Adapt.screenW(),
-                  height: Adapt.screenH() - Adapt.px(90.0),
-                  child: Offstage(
-                    offstage: showDropMenu,
-                    child: AnimatedOpacity(
-                      opacity: showDropMenu ? 0.0 : 1.0,
-                      duration: Duration(milliseconds: 300),
-                      child: DropMenu(),
+                    Positioned(
+                      top: Adapt.px(0),
+                      left: 0,
+                      width: Adapt.screenW(),
+                      height: Adapt.screenH() - Adapt.px(90.0),
+                      child: Offstage(
+                        offstage: showDropMenu,
+                        child: AnimatedOpacity(
+                          opacity: showDropMenu ? 0.0 : 1.0,
+                          duration: Duration(milliseconds: 300),
+                          child: DropMenu(),
+                        ),
+                      ),
                     ),
-                  ),
+                  ],
                 ),
-              ],
+              ),
             ),
           );
         },
         converter: (store) => store.state.showDropMenu,
       ) ,
       floatingActionButton: !showToTopBtn ? null : CustomFloatButton(controller: _controller,)
+    );
+  }
+
+  /// 每一行的介绍
+  Widget sigleRowIntro(offState,title,content) {
+    var direction = Axis.horizontal;
+    List<Widget> items = [Text('$content')];
+    switch(title) {
+      case '◎主 演' :
+        direction = Axis.vertical;
+        var content2 = content.map<Widget>((item)=> Text('$item'));
+        items =<Widget>[
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: content2.toList(),
+          ),
+        ];
+        break;
+    }
+    return Offstage(
+      offstage: offState,
+        child:   Stack(
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.only(left: Adapt.px(200.0)),
+              constraints: BoxConstraints(
+                minHeight: Adapt.px(55.0),
+              ),
+              child: Wrap(
+                direction: direction,
+                crossAxisAlignment: WrapCrossAlignment.center,
+                runAlignment: WrapAlignment.start,
+                alignment:WrapAlignment.center,
+                children: items,
+              ),
+            ),
+            Positioned(
+              left: Adapt.px(0.0),
+              top: 0,
+              child: Container(
+                width: Adapt.px(200.0),
+                height: Adapt.px(55.0),
+                child: Text('$title:',textAlign: TextAlign.start,),
+              ),
+            ),
+          ],
+        ),
     );
   }
 }
